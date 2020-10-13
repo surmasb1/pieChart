f5a5712d47c52428ee7ec2fa77e6786483968828
surmasb1 <surmasb1@gmail.com>
2020-10-13 19:50:25 +0300

    5 commit

--- src/component/PieChart/PieChart.js
+++ src/component/PieChart/PieChart.js
@@ -46,16 +46,18 @@ const {fruit} = state
         }
         arr.push(alpha)
     })
-    const hideTooltip = ()=>{
-        const elem = idRef.current;
-        idRef.current.style.position = "none"
-
-    }
+    // const hideTooltip = ()=>{
+    //     const elem = idRef.current;
+    //     elem.innerHTML = ''
+    //
+    // }
  const showName = (e,title)=>{
          const elem = idRef.current;
          const {pageY, pageX} = e;
          elem.innerHTML = title;
+
      idRef.current.style.position = "absolute"
+     // idRef.current.style.display = "block"
      idRef.current.style.top = `${pageY-20}px`;
      idRef.current.style.left = `${pageX}px`;
 
@@ -69,14 +71,14 @@ const {fruit} = state
             <div>
 
                 <svg height="600" width="600"  viewBox="-100 -100 200 200">
-                    <g transform="rotate(-90)">
+                    <g  transform="rotate(-90)">
                         {arr.map((n , index)=>{
                             let start = `L100,0`;
                             let curentxy = index +1 === arr.length ?`100,0` : `${n.x},${n.y}`
                             let dd= `M0,0 ${index === 0 ? start : "L"+arr[index-1].x+","+arr[index-1].y } A100,100 0 ${fruit[index].count/totalcount >= 0.5 ? 1: 0 },1 ${curentxy} Z`
                             let colorel = `rgb(${255 * Math.random()}, ${255 * Math.random()},${255 * Math.random()})`
                             return(
-                                <path key={index} d={dd} fill={colorel} fillOpacity='0.7' onMouseOut={hideTooltip} onMouseOver={(event)=>{
+                                <path key={index} d={dd} fill={colorel} fillOpacity='0.7'  onMouseOver={(event)=>{
 
                                     showName(event,fruit[index].name)
                                 }}/>
